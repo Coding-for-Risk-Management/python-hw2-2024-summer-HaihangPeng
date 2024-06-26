d4dea9f156dac8c07e800a5dc865cd4479169fc7
HaihangPeng <hp2658@columbia.edu>
2024-06-26 08:38:05 +0800

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -7,7 +7,7 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    numvec = np.arange(start, finish)
+    numvec = np.arange(start, finish+1)
     objvec = np.array(numvec, dtype=object)
     
     objvec[(numvec % 3 == 0) & (numvec % 5 == 0)] = "fizzbuzz"
